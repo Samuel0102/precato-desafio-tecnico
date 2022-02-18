fee25166edcf8f45bc4194fd23dd5db5d0d8481a
Samuel0102 <fsamuelpacheco@gmail.com>
2022-02-18 14:54:07 -0300

    adicionando animacoes de scroll, minificacao de js e de imagens

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const {src, dest, watch, series} = require('gulp');
 const gulpSass = require("gulp-sass")(require("node-sass"));
 const uglifyCss = require('gulp-uglifycss');
 const minifyHtml = require('gulp-htmlmin');
+const uglifyJs = require('gulp-terser');
+const minifyImage = require('gulp-imagemin');
 
 function sass(){
     return src(['./src/style.scss'])
@@ -16,8 +18,21 @@ function html(){
     .pipe(dest('./dist'))
 }
 
-exports.build = series(sass, html)
+function js(){
+    return src('./src/**/*.js')
+    .pipe(uglifyJs())
+    .pipe(dest('./dist'))
+}
+
+function imgMinify() {
+    return gulp.src("src/img/**/*")
+    .pipe(img())
+    .pipe(gulp.dest("dist/images"));
+  }
+
+exports.build = series(sass, html, js, img)
 exports.default = function(){
     watch('./src/**/*.scss', sass);
     watch('./src/**/*.html', html);
+    watch('./src/**/*.js', js)
 }
